9da498f10ff73cb656a4d4f7610c80c5d863a197
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-11-17 22:14:35 +0100

    Created basic structure with logic for sidebar

---
+++ components/Layout.tsx
@@ -0,0 +1,42 @@
+import React, { ReactNode, useState } from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Sidebar from "./Sidebar";
+
+type LayoutProps = {
+  children?: ReactNode;
+  isSidebarOpen: boolean;
+};
+
+export const siteTitle = "stefano || Front-End Developer";
+
+const Layout: React.FunctionComponent<LayoutProps> = ({ children }) => {
+  const [isSidebarOpen, setSidebarOpen] = useState<boolean>(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
+  console.log(isSidebarOpen, "sidebar open");
+
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta name="og:title" content="" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <Navbar isSidebarOpen={isSidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <Sidebar isSidebarOpen={isSidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
